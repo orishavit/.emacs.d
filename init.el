f1698ce00af1754c31da4c691616998c09d7fb21
Ori Shavit <orishavit@gmail.com>
2016-12-04 01:50:07 +0200

    helm-buffers

--- init.el
+++ init.el
@@ -23,7 +23,8 @@
 (use-package helm)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (global-set-key (kbd "M-x") #'helm-M-x)
-
+(global-set-key (kbd "C-x b") #'helm-buffers-list)
+(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
 ;;; projectile
 (use-package projectile
   :config (projectile-global-mode t))
